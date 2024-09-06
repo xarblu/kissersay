81c4d04cfe3a6c2dffedd2283494794e6397f3e8
Xarblu <xarblu@protonmail.com>
2024-09-06 18:50:00 +0200

    more kissers

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,26 @@ use std::io::{self, BufRead, BufReader};
 use std::cmp::Ordering::Greater;
 use clap::Parser;
 
-const KISSER: &str = 
-"⠀⡿⠀⠀⠀⠀⠀⠈⢷⡀⢻⡀⠀⠀⠙⢦⣰⠏⠀⠀⠀⠀⠀⠀⢸⠀
-⠀⡇⠀⠀⠀⠀⠀⠀⢀⣻⠞⠛⠀⠀⠀⠀⠻⠀⠀⠀⠀⠀⠀⠀⢸⠀
-⠀⡇⠀⠀⠀⠀⠀⠀⠛⠓⠒⠓⠓⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀
-⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠀
-⠀⢿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⠀⠀⢀⡟⠀
-⠀⠘⣇⠀⠘⣿⠋⢹⠛⣿⡇⠀⠀⠀⠀⣿⣿⡇⠀⢳⠉⠀⣠⡾⠁⠀
-⣦⣤⣽⣆⢀⡇⠀⢸⡇⣾⡇⠀⠀⠀⠀⣿⣿⡷⠀⢸⡇⠐⠛⠛⣿⠀
-⠹⣦⠀⠀⠸⡇⠀⠸⣿⡿⠁⢀⡀⠀⠀⠿⠿⠃⠀⢸⠇⠀⢀⡾⠁⠀
-⠀⠈⡿⢠⢶⣡⡄⠀⠀⠀⠀⠉⠁⠀⠀⠀⠀⠀⣴⣧⠆⠀⢻⡄⠀⠀
-⠀⢸⠃⠀⠘⠉⠀⠀⠀⠠⣄⡴⠲⠶⠴⠃⠀⠀⠀⠉⡀⠀⠀⢻⡄⠀
-⠀⠘⠒⠒⠻⢦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⠞⠛⠒⠛⠋⠁⠀
-⠀⠀⠀⠀⠀⠀⠸⣟⠓⠒⠂⠀⠀⠀⠀⠀⠈⢷⡀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠙⣦⠀⠀⠀⠀⠀⠀⠀⠀⠈⢷⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⣼⣃⡀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣆⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠉⣹⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡆
-";
+mod kissers;
 
 // let boykisser say stuff
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
     // the message
-    #[arg(index = 1, default_value_t = String::new())]
+    #[arg(index = 1, default_value_t = String::from("-"), help = "What to say (- reads from stdin)")]
     msg: String,
+
+    // which kisser
+    #[arg(short, long, default_value_t = String::from("boykisser"), help = format!("One of: {}", kissers::KISSERS))]
+    kisser: String,
+}
+
+// append "s" with "append" repeated "repeat" times
+fn append_string_repeat(s: &mut String, append: &str, repeat: usize) {
+    for _ in 0..repeat {
+        s.push_str(append);
+    }
 }
 
 // get width of the biggest line in s
@@ -43,16 +37,10 @@ fn max_line_width(s: &str) -> usize {
     return max;
 }
 
-fn append_string_repeat(s: &mut String, append: &str, repeat: usize) {
-    for _ in 0..repeat {
-        s.push_str(append);
-    }
-}
-
 // format the msg
-fn format_msg(msg: &str) -> String {
+fn format_msg(msg: &str, kisser: &str) -> String {
     let msg_max_width: usize = max_line_width(&msg);
-    let indent: usize = max_line_width(&KISSER) / 2;
+    let indent: usize = max_line_width(&kisser) / 2;
     
     // create a msg box
     let mut msg_fmt: String = String::new();
@@ -90,7 +78,7 @@ fn format_msg(msg: &str) -> String {
     msg_fmt.push_str("|/\n\n");
 
     // add character
-    msg_fmt.push_str(KISSER);
+    msg_fmt.push_str(kisser);
 
     return msg_fmt
 }
@@ -100,16 +88,23 @@ fn main() {
 
     let mut msg = String::new();
     
-    if args.msg.len() > 0 {
-        msg = args.msg;
-    } else {
+    if args.msg == "-" {
         let reader = BufReader::new(io::stdin());
 
         for line in reader.lines() {
             msg.push_str(&line.unwrap());
             msg.push_str("\n");
         }
+    } else {
+        msg = args.msg;
     }
 
-    print!("{}", format_msg(&msg));
+    let kisser = kissers::get_kisser(&args.kisser);
+
+    if kisser.len() == 0 {
+        println!("Invalid kisser - possible values: {}", kissers::KISSERS);
+        return;
+    }
+
+    print!("{}", format_msg(&msg, &kisser));
 }
